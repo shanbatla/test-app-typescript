b398be9a20314bf445638e5cfa14b7dca28e981a
Shan Batla <shan.batla@gmail.com>
2021-08-30 15:06:17 -0400

    Remove styles from header component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,15 @@ import logo from '../logo.svg';
 
 const Header = () => {
     return (
-        <header className="App-header">
+        <header>
             <h1>Hello from Shan</h1>
-            <img src={logo} className="App-logo" alt="logo" />
             <p>
                 Edit <code>src/App.tsx</code> and save to reload.
             </p>
-            <a
-                className="App-link"
+            <a className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
+                rel="noopener noreferrer">
             </a>
         </header>
     )
