77eab4b5dfb06ccdba2c0f260aad91485a1daa92
Shan Batla <shan.batla@gmail.com>
2021-08-30 16:22:09 -0400

    Add lib dir and constants file.

---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+const constants = {
+  usersUrl: 'https://jsonplaceholder.typicode.com/users',
+  imageUrl: 'https://i.pinimg.com/originals/b0/2d/5e/b02d5efd37c7d2dd3f71d874d6bd15a7.jpg'
+}
+
+export default constants
